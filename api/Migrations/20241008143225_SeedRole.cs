e7a04cf114788e2fa80ec723e5890bad4f50a969
Trần Thành Đạt <tthanhdat05102003@gmail.com>
2024-10-08 21:39:58 +0700

    Install Identity + Register

---
+++ api/Migrations/20241008143225_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "d3962b43-3f6e-402c-9abd-1a0da1a26071", null, "Admin", "ADMIN" },
+                    { "e66b3ef3-9ab5-48f9-bf12-786465537b1b", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d3962b43-3f6e-402c-9abd-1a0da1a26071");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e66b3ef3-9ab5-48f9-bf12-786465537b1b");
+        }
+    }
+}
